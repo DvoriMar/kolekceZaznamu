a25e1cd4aea55641f4a7ad63772c1759f55712f0
DvoriMar <dvoracek_martin@oauh.cz>
2023-01-19 23:00:10 +0100

    Přidán .gitignore a Main.java a Zakaznik.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+
+    private static FileWriter writer;
+
+    public static final String nazevSouboru = "kolekce.txt";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> seznam = new ArrayList<>();
+
+        try{
+            FileWriter writer = new FileWriter("kolekce.txt");
+            writer.write("Karel Dvořák" + "\n" +"Milada Dvořáková" + "\n" + "Karel Heřman");
+            writer.close();
+        }
+
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader(nazevSouboru))))
+        {
+
+            while (scanner.hasNextLine()) {
+                String prectenyRadek = scanner.nextLine();
+                seznam.add(prectenyRadek);
+            }
+
+        }
+
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        seznam.forEach(System.out::println);
+
     }
 }
--- src/Zakaznik.java
+++ src/Zakaznik.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Zakaznik {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zakaznik {
+    private String jmeno;
+    private int pocetProdeju;
+
+    public Zakaznik(String jmeno, int pocetProdeju) {
+        this.jmeno = jmeno;
+        this.pocetProdeju = pocetProdeju;
+    }
+
+    public String getJméno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public int getPocetProdeju() {
+        return pocetProdeju;
+    }
+
+    public void setPocetProdeju(int pocetProdeju) {
+        this.pocetProdeju = pocetProdeju;
+    }
 }
